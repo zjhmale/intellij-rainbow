47cb5bdafb7f45c7978d7fa06be62dd74888001e
zjhmale <zjhmale@gmail.com>
2016-02-23 00:53:33 +0800

    make rainbow more colorful

---
+++ src/zjhmale/rainbow/RainbowColors.java
@@ -0,0 +1,100 @@
+package zjhmale.rainbow;
+
+import java.awt.*;
+
+/**
+ * Created by zjh on 16/2/22.
+ */
+public class RainbowColors {
+    public static final Color[] LIGHT_COLORS = {
+            new Color(120,104,63),
+            new Color(67,120,63),
+            new Color(63,113,120),
+            new Color(81,63,120),
+            new Color(120,63,90),
+            new Color(112,126,79),
+            new Color(79,126,103),
+            new Color(79,92,126),
+            new Color(122,79,126),
+            new Color(126,84,79),
+            new Color(120,55,120),
+            new Color(120,68,55),
+            new Color(94,120,55),
+            new Color(55,120,94),
+            new Color(55,68,120)
+    };
+
+    public static final Color[] LIGHT_COMMON_COLORS = {
+            new Color(120,104,63),
+            new Color(67,120,63),
+            new Color(63,113,120),
+            new Color(81,63,120),
+            new Color(120,63,90),
+            new Color(112,126,79),
+            new Color(79,126,103),
+            new Color(79,92,126),
+            new Color(122,79,126),
+            new Color(126,84,79),
+            new Color(120,55,120),
+            new Color(120,68,55),
+            new Color(94,120,55),
+            new Color(55,120,94),
+            new Color(55,68,120),
+            new Color(0,0,139),
+            new Color(102,102,153),
+            new Color(101,67,33),
+            new Color(0,139,139),
+            new Color(255,140,0),
+            new Color(83,104,120),
+            new Color(204,78,92),
+            new Color(47,79,79),
+            new Color(145,129,81),
+            new Color(23,114,69)
+    };
+
+    public static final Color[] DARK_COLORS = {
+            new Color(153,153,187),
+            new Color(187,153,180),
+            new Color(187,166,153),
+            new Color(166,187,153),
+            new Color(153,187,180),
+            new Color(224,208,160),
+            new Color(163,224,160),
+            new Color(160,214,224),
+            new Color(182,160,224),
+            new Color(224,160,188),
+            new Color(167,192,185),
+            new Color(167,170,192),
+            new Color(192,167,189),
+            new Color(192,175,167),
+            new Color(179,192,167)
+    };
+
+    public static final Color[] DARK_COMMON_COLORS = {
+            new Color(114,213,114),
+            new Color(197,225,165),
+            new Color(230,238,156),
+            new Color(255,245,157),
+            new Color(255,224,130),
+            new Color(255,204,128),
+            new Color(255,171,145),
+            new Color(188,170,164),
+            new Color(176,190,197),
+            new Color(255,167,38),
+            new Color(255,138,101),
+            new Color(249,189,187),
+            new Color(248,187,208),
+            new Color(225,190,231),
+            new Color(209,196,233),
+            new Color(255,224,178),
+            new Color(197,202,233),
+            new Color(208,217,255),
+            new Color(179,229,252),
+            new Color(178,235,242),
+            new Color(178,223,219),
+            new Color(163,233,164),
+            new Color(220,237,200),
+            new Color(240,244,195),
+            new Color(255,183,77)
+    };
+}
--- src/zjhmale/rainbow/RainbowIdentifierHighliter.java
+++ src/zjhmale/rainbow/RainbowIdentifierHighliter.java
@@ -22,46 +22,8 @@ import java.util.List;
  * Created by zjh on 2016/2/13.
  */
 public class RainbowIdentifierHighliter implements Annotator {
-    private static int rainbowIdentifierFaceCount = 15;
-
     private static String[] parentheses = {"(", ")", "{", "}", "[", "]"};
     private static List<String> parenthesesList = Arrays.asList(parentheses);
-
-    private static final Color[] LIGHT_COLORS = {
-            new Color(120,104,63),
-            new Color(67,120,63),
-            new Color(63,113,120),
-            new Color(81,63,120),
-            new Color(120,63,90),
-            new Color(112,126,79),
-            new Color(79,126,103),
-            new Color(79,92,126),
-            new Color(122,79,126),
-            new Color(126,84,79),
-            new Color(120,55,120),
-            new Color(120,68,55),
-            new Color(94,120,55),
-            new Color(55,120,94),
-            new Color(55,68,120)
-    };
-
-    private static final Color[] DARK_COLORS = {
-            new Color(153,153,187),
-            new Color(187,153,180),
-            new Color(187,166,153),
-            new Color(166,187,153),
-            new Color(153,187,180),
-            new Color(224,208,160),
-            new Color(163,224,160),
-            new Color(160,214,224),
-            new Color(182,160,224),
-            new Color(224,160,188),
-            new Color(167,192,185),
-            new Color(167,170,192),
-            new Color(192,167,189),
-            new Color(192,175,167),
-            new Color(179,192,167)
-    };
 
     /**
      *
@@ -72,9 +34,9 @@ public class RainbowIdentifierHighliter implements Annotator {
     public static TextAttributes getBraceAttributes(String identifier, Color background) {
         Color rainbowColor;
         if (background.getRed() < 128 && background.getGreen() < 128 && background.getBlue() < 128) {
-            rainbowColor = DARK_COLORS[HashFace.rainbowIdentifierHash(identifier) % rainbowIdentifierFaceCount];
+            rainbowColor = RainbowColors.DARK_COMMON_COLORS[HashFace.rainbowIdentifierHash(identifier) % RainbowColors.DARK_COMMON_COLORS.length];
         } else {
-            rainbowColor = LIGHT_COLORS[HashFace.rainbowIdentifierHash(identifier) % rainbowIdentifierFaceCount];
+            rainbowColor = RainbowColors.LIGHT_COMMON_COLORS[HashFace.rainbowIdentifierHash(identifier) % RainbowColors.LIGHT_COMMON_COLORS.length];
         }
         return new TextAttributes(rainbowColor, null, null, null, Font.PLAIN);
     }
